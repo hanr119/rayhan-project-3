1b567defc3351545570e9e5c02ced62cb02c5b4e
hanr119 <70984148+hanr119@users.noreply.github.com>
2021-04-26 22:31:52 -0400

    tweaks w/ js

--- navigation.js
+++ navigation.js
@@ -1,29 +1,50 @@
-
 var submitButton = document.querySelector(".submit-button")
-
-var formInput = document.querySelector("input[value=type-continent-name]")
-
-submitButton.addEventListener("click", function(event){
-    event.preventDefault();
-    if (formInput.value == 'north america' || 'North America' || 'NORTH AMERICA') {
-        window.location.href = "./continents/north-america.html"
-    }
-    if (formInput.value == 'africa' || 'Africa' || 'AFRICA') {
-        window.location.href = "./continents/africa.html"
-    }
-    if (formInput.value == 'asia' || 'Asia' || 'ASIA') {
-        window.location.href = "./continents/asia.html"
-    }
-    if (formInput.value == 'australia' || 'Australia' || 'AUSTRALIA') {
-        window.location.href = "./continents/australia.html"
-    }
-    if (formInput.value == 'antarctica' || 'Antarctica' || 'ANTARCTICA') {
-        window.location.href = "./continents/antarctica.html"
-    }
-    if (formInput.value == 'europe' || 'Europe' || 'EUROPE') {
-        window.location.href = "./continents/europe.html"
-    }
-    if (formInput.value == 'south america' || 'South America' || 'SOUTH AMERICA') {
-        window.location.href = "./continents/south-america.html"
+var formInput = document.querySelector(".continent-input")
+// listen for key press event on the input
+formInput.addEventListener("keypress", function(event) {
+    // run this code if enter key is pressed
+    if(event.key === 'Enter') {
+        // prevent page reload
+        event.preventDefault();
+        // run the continent checking function
+        checkContinentValue()       
     }
 })
+// listen for click on button
+submitButton.addEventListener("click", function(event){
+    // stop the page from refreshing
+    event.preventDefault();
+    // run the continent checking function
+    checkContinentValue()
+})
+// this function checks to see if value entered matches continent
+function checkContinentValue() {
+  // a new variable that converts any submitted value to uppercase to match queries
+  let newValue = formInput.value.toUpperCase();
+  // log this new value to test
+  console.log(newValue)
+  if (newValue.includes("NORTH AMERICA")) {
+      window.location.href = "./continents/north-america.html"
+  }
+  else if (newValue.includes("AFRICA")) {
+      window.location.href = "./continents/africa.html"
+  }
+  else if (newValue.includes("ASIA")) {
+      window.location.href = "./continents/asia.html"
+  }
+  else if (newValue.includes("AUSTRALIA")) {
+      window.location.href = "./continents/australia.html"
+  }
+  else if (newValue.includes("ANTARCTICA")) {
+      window.location.href = "./continents/antarctica.html"
+  }
+  else if (newValue.includes("EUROPE")) {
+      window.location.href = "./continents/europe.html"
+  }
+  else if (newValue.includes("SOUTH AMERICA")) {
+      window.location.href = "./continents/south-america.html"
+  }
+  else {
+      alert("Not a continent, try again")
+  }
+}
